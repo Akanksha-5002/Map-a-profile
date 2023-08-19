3932369e35b3a048f65180e192cba69cfb926684
Akanksha-5002 <akanksha.indalkar@gmail.com>
2023-08-19 22:39:23 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,112 @@
+import { Component } from '@angular/core';
+
+declare const L: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'maprofile';
+
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = position.coords;
+      //const latLong = [coords.latitude, coords.longitude];
+      const latLong = [12.971599 , 77.594566];
+      //console.log(
+       // `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+     // );
+      let mymap = L.map('map').setView(latLong, 13);
+
+      L.tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWthbmtzaGEtMDUiLCJhIjoiY2xsZ3oxeWhjMWE3MzNzbWc1bmpqY2tzZyJ9.hATGvsPYQy-GH73f7--ePw',
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token',
+        }
+      ).addTo(mymap);
+
+      let marker = L.marker(latLong).addTo(mymap);
+
+      marker.bindPopup('<b>Hi</b>').openPopup();
+
+      let popup = L.popup()
+        .setLatLng(latLong)
+        .setContent('Adams Baker')
+        .openOn(mymap);
+    });
+    this.watchPosition();
+  }
+
+  ngOnInit1() {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = position.coords;
+      const latLong = [coords.latitude, coords.longitude];
+      console.log(
+        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+      );
+      let mymap = L.map('map').setView(latLong, 13);
+
+      L.tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWthbmtzaGEtMDUiLCJhIjoiY2xsZ3oxeWhjMWE3MzNzbWc1bmpqY2tzZyJ9.hATGvsPYQy-GH73f7--ePw',
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token',
+        }
+      ).addTo(mymap);
+
+      let marker = L.marker(latLong).addTo(mymap);
+
+      marker.bindPopup('<b>Hi</b>').openPopup();
+
+      let popup = L.popup()
+        .setLatLng(latLong)
+        .setContent('Adams Baker')
+        .openOn(mymap);
+    });
+    this.watchPosition();
+  }
+ 
+  watchPosition() {
+    let desLat = 0;
+    let desLon = 0;
+    let id = navigator.geolocation.watchPosition(
+      (position) => {
+        console.log(
+          `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+        );
+        if (position.coords.latitude === desLat) {
+          navigator.geolocation.clearWatch(id);
+        }
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  }
+}
+
+
